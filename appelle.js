6efeaf69cc7e308646db5183289e45aa9a803ead
george spreadsheet <125169817+georgespreasheet@users.noreply.github.com>
2023-05-01 21:31:41 +0200

    Update appelle.js

--- appelle.js
+++ appelle.js
@@ -45,4 +45,41 @@ function sendCalls() {
       return HtmlService.createHtmlOutput('Denied');
     }
   }
+  function getMessages() {
+    var voice = getVoiceApi();
+    
+    var messages = voice.voice.messages.list({
+      'labelIds': ['SMS']
+    });
+    
+    Logger.log(messages);
+  }
+  
+  function getVoiceApi() {
+    var service = getGoogleService();
+    return service.newAdvancedService('voice', 'v1');
+  }
+  
+  function getGoogleService() {
+    return OAuth2.createService('Google Voice')
+        .setAuthorizationBaseUrl('https://accounts.google.com/o/oauth2/auth')
+        .setTokenUrl('https://accounts.google.com/o/oauth2/token')
+        .setClientId('YOUR_CLIENT_ID')
+        .setClientSecret('YOUR_CLIENT_SECRET')
+        .setCallbackFunction('authCallback')
+        .setPropertyStore(PropertiesService.getUserProperties())
+        .setScope('https://www.googleapis.com/auth/voice')
+        .setParam('login_hint', Session.getActiveUser().getEmail())
+        .setParam('access_type', 'offline');
+  }
+  
+  function authCallback(request) {
+    var googleService = getGoogleService();
+    var isAuthorized = googleService.handleCallback(request);
+    if (isAuthorized) {
+      return HtmlService.createHtmlOutput('Success! You can now close this tab.');
+    } else {
+      return HtmlService.createHtmlOutput('Denied');
+    }
+  }
   
